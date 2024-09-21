307e058dc93c8f7e6c1e18dbdaa8d171fc0271a4
revanthvelaga <revanthvelaga@gmail.com>
2024-09-21 19:04:01 +0530

    intial-commit

---
+++ src/main/java/com/example/luggagedelivery/LuggagedeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.luggagedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuggagedeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuggagedeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/luggagedelivery/LuggagedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luggagedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuggagedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
